9515ae8a87c8997770f3c8976cd394ed0b985a82
ShanInfotechSolutions <shanmukh@shaninfotechsolutions.com>
2025-05-12 18:06:18 +0530

    helloWorld

---
+++ src/com/shanInfotech/jenkinsExample/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.shanInfotech.jenkinsExample;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Ganesh! Welcome To Jenkins World");
+	}
+
+}
